7125e50effd3c5b8ddfe3f05e87472e24a2c7340
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2023-04-11 13:24:35 -0300

    wip

--- test/Gate.t.sol
+++ test/Gate.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.7;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
-import {Gate} from "../src/Gate.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+import {Gate, IGuardian} from "../src/Gate.sol";
 
 contract GateTest is Test {
     Gate private challenge;
 
-    address hacker = makeAddr("hacker");
+    address private hacker = makeAddr("hacker");
 
     function setUp() external {
         challenge = new Gate();
@@ -17,13 +19,19 @@ contract GateTest is Test {
 
     function test_hack() external {
         /**
-      Objective of CTF
+            Objective of CTF
 
-      You need to set the opened flag to true via the open function
-      You need to handwrite the bytecode opcode by opcode and stay within the size of less than 33 bytes    
-     */
+            You need to set the opened flag to true via the open function
+            You need to handwrite the bytecode opcode by opcode and stay within the size of less than 33 bytes    
+        */
+        IGuardian attacker = IGuardian(
+            HuffDeployer.config().deploy("GateAttacker")
+        );
 
         vm.startPrank(hacker, hacker);
+        challenge.open(address(attacker));
         vm.stopPrank();
+
+        assertTrue(challenge.opened());
     }
 }
